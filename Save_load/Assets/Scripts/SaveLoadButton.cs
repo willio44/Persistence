91a03018d602201f78259b90bb998c3afd0e795c
B533P-27\533 <william.chiasson@hotmail.ca>
2018-11-12 16:04:39 -0500

    commit save

---
+++ Save_load/Assets/Scripts/SaveLoadButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadButton : MonoBehaviour {
+
+	// Use this for initialization
+public 	void Save () {
+        GameController.control.SaveGame();
+        SceneController.sceneControl.SaveScene();
+	}
+	
+	// Update is called once per frame
+public 	void Load () {
+        GameController.control.LoadGame();
+        SceneController.sceneControl.LoadScene();
+	}
+}
--- Save_load/Assets/Scripts/SceneController.cs
+++ Save_load/Assets/Scripts/SceneController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class SceneController : MonoBehaviour {
 
@@ -9,11 +12,13 @@ public class SceneController : MonoBehaviour {
 
     public float health;
     public float experience;
+    public int scene;
 
     private void Awake() {
         if(sceneControl == null) {
             DontDestroyOnLoad(gameObject);
             sceneControl = this;
+            LoadScene();
         } else if(sceneControl != this) {
             Destroy(gameObject);
         }
@@ -23,6 +28,8 @@ public class SceneController : MonoBehaviour {
         if ((SceneManager.sceneCountInBuildSettings-1) > SceneManager.GetActiveScene().buildIndex) {
             print("loading " + (SceneManager.GetActiveScene().buildIndex + 1));
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            
+            
         } else {
             print("This is the last scene");
         }
@@ -43,6 +50,51 @@ public class SceneController : MonoBehaviour {
         style.fontSize = 56;
         GUI.Label(new Rect(10, 10, 180, 80), "Active scene index : "  + SceneManager.GetActiveScene().buildIndex, style);
     }
-   
+
+    public void SaveScene()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+
+        FileStream file = File.Open(Application.persistentDataPath + "/sceneInfo.dat", FileMode.Create);
+        SaveData sceneData = new SaveData();
+        //.sceneSave = scene;
+        sceneData.sceneSave = SceneManager.GetActiveScene().buildIndex;
+
+
+
+        bf.Serialize(file, sceneData);
+        file.Close();
+    }
+
+
+    public void LoadScene()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        if (!File.Exists(Application.persistentDataPath + "/sceneInfo.dat"))
+        {
+            throw new Exception("Game file not existing");
+        }
+
+
+
+
+        FileStream file = File.Open(Application.persistentDataPath + "/sceneInfo.dat", FileMode.Open);
+        SaveData sceneData = (SaveData)bf.Deserialize(file);
+        file.Close();
+        //scene = sceneData.sceneSave;
+        SceneManager.LoadScene(sceneData.sceneSave);
+       
+
+
+    }
+
+
 
 }
+[Serializable]
+class SaveData
+{
+    public int sceneSave;
+
+}
+
